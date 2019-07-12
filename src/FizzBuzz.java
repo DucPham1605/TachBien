0d8466ef995c2bc8659947fddc44a498846fedbe
ducp1605 <ducp1605@gmail.com>
2019-07-12 15:01:57 +0700

    Uploaded

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,12 +1,14 @@
 public class FizzBuzz {
     public static String fizzBuzz(int number){
-        if(number % 3 == 0 && number % 5 == 0)
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+        if(isFizz && isBuzz)
             return "FizzBuzz";
+        if(isFizz)
 
-        if(number % 3 == 0)
             return "Fizz";
 
-        if(number % 5 == 0)
+        if(isBuzz)
             return "Buzz";
 
         return number + "";
